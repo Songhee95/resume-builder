074f30b7b65374dc7cab7e57171ac0e32b834f61
songhee <thdgml5537@gmail.com>
2021-02-24 21:46:18 -0500

    add login fail error message

--- public/js/login.js
+++ public/js/login.js
@@ -1,22 +1,29 @@
-$(document).ready(function(){
-    $("form.login").on("submit", function(event){
-        event.preventDefault();
-        let userEmail = $(".userEmail").val();
-        let userPwd = $(".userPassword").val();
-        
-        //verify the user 
-        $.ajax({
-            method: "POST",
-            url: "/api/login",
-            data: {
-                email: userEmail,
-                password: userPwd
-            }
-        }).then(function(){
-            console.log("data sent!");
-            window.location.replace("/index");
-        }).catch(err =>{
-            console.log(err);
-        })
+$(document).ready(function () {
+  $("form.login").on("submit", function (event) {
+    event.preventDefault();
+    let userEmail = $(".userEmail").val();
+    let userPwd = $(".userPassword").val();
+
+    //verify the user
+    $.ajax({
+      method: "POST",
+      url: "/api/login",
+      data: {
+        email: userEmail,
+        password: userPwd,
+      },
     })
-})
+      .then(function () {
+        console.log("data sent!");
+        window.location.replace("/index");
+      })
+      .catch((err) => {
+        event.preventDefault();
+        $(".login__error__message")
+          .removeClass("is-hidden")
+          .addClass("is-inline");
+
+        console.log(err);
+      });
+  });
+});
--- public/js/signin.js
+++ public/js/signin.js
@@ -1,29 +1,55 @@
-$(document).ready(function(){
-    $("form.signIn").on("submit", function(event){
-        event.preventDefault();
-        const userName = $(".userName").val();
-        const phoneNumber = $('.phoneNumber').val();
-        const userEmail = $(".userEmail").val();
-        const userPassword = $('.userPassword').val();
-        userSignIn(userName, userPassword, userEmail, phoneNumber);
-        $(".userName").val("");
-        $(".phoneNumber").val("");
-        $(".userEmail").val("");
-        $(".userPassword").val("");
+$(document).ready(function () {
+  let emailStatus = false;
+  let passwordStatus = false;
+
+  $("form.signIn").on("submit", function (event) {
+    event.preventDefault();
+    const userName = $(".userName").val();
+    const phoneNumber = $(".phoneNumber").val();
+    const userEmail = $(".userEmail").val();
+    const userPassword = $(".userPassword").val();
+    userSignIn(userName, userPassword, userEmail, phoneNumber);
+    $(".userName").val("");
+    $(".phoneNumber").val("");
+    $(".userEmail").val("");
+    $(".userPassword").val("");
+  });
+  function userSignIn(userName, userPassword, userEmail, phoneNumber) {
+    $.post("/api/signin", {
+      userName: userName,
+      password: userPassword,
+      email: userEmail,
+      phone: phoneNumber,
     })
-    function userSignIn(userName, userPassword, userEmail, phoneNumber){
-        $.post("/api/signin", {
-            userName : userName,
-            password: userPassword,
-            email: userEmail,
-            phone: phoneNumber
-        })
-        .then(function(data){
-            console.log(data)
-            window.location.replace("/");
-        })
-        .catch(err=>{
-            console.log(err)
-        });
+      .then(function (data) {
+        console.log(data);
+        window.location.replace("/");
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  $(".userEmail").change(function () {
+    if (
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        this.value
+      )
+    ) {
+      $(".email__check__icon").css("color", "green");
+      $(".userEmail").css("border", "1px solid green");
+    } else {
+      $(".email__check__icon").css("color", "red");
+      $(".userEmail").css("border", "1px solid red");
     }
-})
+  });
+
+  $(".userPassword").change(function () {
+    if (this.value.length < 8) {
+      $(".password__check__icon").css("color", "red");
+      $(".userPassword").css("border", "1px solid red");
+    } else {
+      $(".password__check__icon").css("color", "green");
+      $(".userPassword").css("border", "1px solid green");
+    }
+  });
+});
