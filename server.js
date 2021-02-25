55a432737c2aefefed7a6c405cb8210ee1198357
songhee <thdgml5537@gmail.com>
2021-02-25 16:05:45 -0500

    heroku deploy

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const {
 const db = require("./models");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
