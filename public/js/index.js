b9ad60e1b53d2042bba5395478689e172435bb63
songhee <thdgml5537@gmail.com>
2021-02-26 22:04:11 -0500

    resume list styling change

--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,12 @@ $(document).ready(function () {
     );
     data.forEach(function (resume) {
       let resumeTile =
-        "<div class='tile is-3 has-text-centered'><div class='tile is-parent'><article class='tile is-child notification is-info'><a class='resumeName' href ='/templateOne/";
+        "<div class='tile is-3 has-text-centered'><div class='tile is-parent'><article class='tile is-child notification is-warning'><a class='resumeName' href ='/templateOne/";
       resumeTile += resume.id;
       resumeTile += "'>";
       resumeTile += resume.resumeName;
       resumeTile +=
-        "</a></p><figure class='image is-4by3'><img src='https://png.pngtree.com/png-clipart/20200709/original/pngtree-elegant-minimalistic-modern-vector-resume-or-cv-template-designed-on-a4-png-image_3528067.jpg'></figure>";
+        "<figure class='image is-3by3'><img src='https://png.pngtree.com/png-clipart/20200709/original/pngtree-elegant-minimalistic-modern-vector-resume-or-cv-template-designed-on-a4-png-image_3528067.jpg'></figure></a>";
       resumeTile += "<div><button class='resumeEditButton' data-id='";
       resumeTile += resume.id;
       resumeTile +=
